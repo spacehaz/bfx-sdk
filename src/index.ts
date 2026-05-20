f4f91411b15275628dbc00a1e3ac5c736d2c1321
Hazo Baikulov <spacehaz@gmail.com>
2026-05-20 12:24:11 +0300

    exporting types and modules

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
-export type { Address, CurveParams, PoolInfo, PoolState, QuoteResult, TransactionRequest } from "./types";
+export type { Address, CurveParams, PoolInfo, PoolState, QuoteHop, QuoteResult, TransactionRequest } from "./types";
+export type { BuildSwapParams } from "./swap";
 export { BFX } from "./pool/index";
 export type { IBFX } from "./pool/index";
+export { ROUTER_ADDRESS, USDC_ADDRESS } from "./constants";
 export { quote } from "./quote";
 export { buildSingleHopSwap, buildMultiHopSwap } from "./swap";
